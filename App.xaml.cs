6039acd2685afb7f1e5aae4fabaab18e7cad37b2
IUSTINA-ALEXANDRA STAN <iustina.stan@stud.ubbcluj.ro>
2024-11-24 18:25:26 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StanIustinaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace StanIustinaLab7
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnDiscoverMoreClicked(object sender, EventArgs e)
+        {
+            // Afișează un mesaj de tip alertă
+            DisplayAlert("Descoperă mai multe", "To be continued...", "OK");
+        }
+    }
+}
+       
+
+
